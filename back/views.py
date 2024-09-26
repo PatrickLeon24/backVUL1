c4a0d12a9239316b1617ddde1ae916459e22e464
PatrickLeon24 <20211454@ALOE.ULIMA.EDU.PE>
2024-09-25 23:11:17 -0500

    Login Prueba

--- back/views.py
+++ back/views.py
@@ -1,8 +1,44 @@
-from django.shortcuts import render
-from django.http import *
-# Create your views here.
-def prueba(request):
-    data = {
-        'nombre': 'Juan'  # El nombre que quieras devolver
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+# Datos de ejemplo de usuarios
+users = [
+    {
+        "id": 1,
+        "email": "123",
+        "password": "123",
+        "nombres": "Daniel Alonso",
+        "apellidos": "Taype Rojas",
+        "DNI": "47245671",
+        "direccion": "av los nogales",
+        "genero": "hombre"
     }
-    return JsonResponse(data)
+]
+
+@csrf_exempt  # Desactiva la protección CSRF para este endpoint
+def prueba(request):
+    if request.method == 'POST':
+        # Obtener los datos del request
+        data = json.loads(request.body)
+        email = data.get('email')
+        password = data.get('password')
+        user = {
+            "id": 1,
+            "email": "123",
+            "password": "123",
+            "nombres": "Daniel Alonso",
+            "apellidos": "Taype Rojas",
+            "DNI": "47245671",
+            "direccion": "av los nogales",
+            "genero": "hombre"
+        }
+    
+        # Validar las credenciales
+       
+        if "123" == email and "123"== password:
+            return JsonResponse(user)  # Devuelve la información del usuario si las credenciales son correctas
+        
+        return JsonResponse({'message': 'Credenciales incorrectas'}, status=400)  # Mensaje de error si las credenciales son incorrectas
+
+    return JsonResponse({'message': 'Método no permitido'}, status=405)  # Mensaje para métodos no permitidos
